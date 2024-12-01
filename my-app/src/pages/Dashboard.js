2b615404499aca6fdf5b17adcc1919836e5d8f91
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-12-01 18:03:38 +0100

    remove unnacassery code on the dashbaord.js

--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -11,8 +11,6 @@ const Dashbaord = () => {
     // empty variable
     // https://react.dev/reference/react/useState
     // set usarname and password are the function s
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
 
     // this are the correct login data
     const corectname = 'uncinc'
@@ -20,25 +18,10 @@ const Dashbaord = () => {
 
     // here is the standard value false but that must be depending on the user login
     // is logged in is the variable
-
-
-    // setIsLoggedIn that is now false but that must be true if the credentials are stored in localStorage
     // setIsLoggedIn that is now false but that must be true if the credentials are stored in localStorage
     let [isLoggedIn, setIsLoggedIn] = useState(false)
 
-    console.log(isLoggedIn +' empty state')//log the false empty state if there is no data in localstorage and
-    // set it to true if there is data
-
-    // here the data is stored if the user has logged in
-    // by application local storage there you can see the data
-    const handleLogin = () => {
-        localStorage.setItem('isLoggedIn',true);
-        localStorage.setItem('user', JSON.stringify(username))
-        localStorage.setItem('password', JSON.stringify(password))
-        setIsLoggedIn(true);//
-
-
-    };
+    console.log(isLoggedIn +' empty state')//log the false empty state if there is no data in localstorage
 
     // https://react.dev/reference/react/useEffect
     // with hooks the useffect checkts if the value is true it is something that checks the state every time and is not a boolean like true or false
@@ -58,43 +41,18 @@ const Dashbaord = () => {
     const handleLogout = () => {
         localStorage.removeItem('uncinc') //remove the username
         localStorage.removeItem('letmein')//remove the password
-        navigate('../pages/Login');
+        navigate('../pages/Login');//navigate to the login page
         localStorage.setItem('isLoggedIn',false);
-        localStorage.clear();
+        localStorage.clear();//remove the credentials
         setIsLoggedIn(false);//set it to false
 
 
     };
 
-
-    // handlesubmit
-
-    function handleSubmit(event) {
-        event.preventDefault()
-        // if islogged seth the value to true
-        if(isLoggedIn) {
-            setIsLoggedIn(true)
-
-        }
-
-        // if the username and password are correct do a callback to the handle login and navigate to dashbaord
-        if (username === corectname && password === corectpassword) {
-            handleLogin ()
-            console.log('you are logged in');
-            navigate('../pages/Dashboard');
-
-        } else {
-            // give a alert incorrect username and passowrd
-            alert("Username and password doesn't exist!");
-            console.log('wrong password');
-        }
-
-    }
     return (
         <main className="">
             <section>
                 <h2>Dashboard</h2>
-
                 {isLoggedIn === true ? (
                     <div>
                         <p>Welkom u bent ingelogd</p>
